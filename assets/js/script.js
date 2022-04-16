c8aa38eff5233699f0a09302a6c31669ca9462b8
MRoccazzella <matias.a.re@hotmail.com>
2022-04-16 15:05:09 -0300

    desafio complementario 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
 alert("Bienvenido al calculo de intereses");
 let monto = parseInt(prompt("Cual es el monto al cual quiere aplicarle un interes?"));
-let interes = parseInt(prompt("Cual es el interes que quiere aplicar?    de 0 a 1000"));
-while(monto < 0){
-    alert("El moto que ingreso es menor a 0")
+while(isNaN(monto) || monto < 0){
+    alert("El valor ingresado no es un numero o bien es un valor negativo.")
     monto = parseInt(prompt("Cual es el monto al cual quiere aplicarle un interes?"));
 }
-while(interes < 0 || interes > 1000){
-    alert("El interes no se encuentra dentro del rango admitido")
-    interes = parseInt(prompt("Cual es el interes que quiere aplicar?    de 0 a 1000"));
+let interes = parseInt(prompt("Cual es el interes que quiere aplicar?     0 a 1000"));
+while(interes < 0 || interes > 1000 || isNaN(interes)){
+    alert("El valor ingresado no es un numero o bien no se encuentra dentro del rango admitido")
+    interes = parseInt(prompt("Cual es el interes que quiere aplicar?     0 a 1000"));
 }
 let total = monto + (monto * interes / 100)
 alert("Al monto de $" + monto + " aplicandole el interes del "+ interes + "% es igual a: $" + total);
