9617142761e282a6fc3aeafc05329fc810267172
MRoccazzella <matias.a.re@hotmail.com>
2022-05-09 19:09:38 -0300

    Entrega eventos clase 9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+
+let body = document.getElementById("body");
+let botonInicioSesion = document.getElementById("botonIniciaSesion")
+botonInicioSesion.onclick = () =>{
+    body.innerHTML = `
+    <div>
+        <h2 class="textoInicioSesion">Inicia Sesion</h2>
+    </div>
+    <div class="contenedorForm">
+        <form id="idFormulario">
+            <div class="mb-3">
+                <label for="name" class="form-label">Ingrese su ID</label>
+                <input type="name" class="form-control" id="exampleInputName">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Clave</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="submit btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+    `
+}
 //FUNCIONES
-function saludo(nombre){
+/*function saludo(nombre){
     alert("Bienvenido " + nombre)
 }
 function opciones(nombre){
@@ -208,4 +233,4 @@ while(valor != 5){
     
 }
 prom = promedioValoraciones(valoracion);
-alert("El usuario " + usuarioFinal.nombre + " valoro el programa con un valor promedio de: " + prom);
+alert("El usuario " + usuarioFinal.nombre + " valoro el programa con un valor promedio de: " + prom);*/
